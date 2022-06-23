dde94503c625412a247dc492d9a33768ffa396fc
Amirsam Goharpey <50692220+amirsamgoharpay@users.noreply.github.com>
2022-06-23 05:25:58 -0700

    Add files via upload

---
+++ numpad.py
@@ -0,0 +1,26 @@
+from random import randint 
+from time import time , sleep
+from os import system
+from colorama import Back, init
+init(autoreset=True)
+tre = 0
+times = int(input("how much number do you want to take for exam : "))
+scor = 0
+system("CLS")
+for i in range(times):
+    num = randint(1000000 , 10000000)
+    print(Back.GREEN + str(num))
+    start = time()
+    inp = int(input('type the num with numpad : '))
+    end = time()
+    est = end - start
+    if num == inp :
+        scor += 1
+        tre += est
+
+    sleep(0.4)
+    system("CLS")
+        
+print(f"your score is : {scor}/{times}")
+print(f"avrage time for typing {scor} num : {tre/scor}")
+input()  
