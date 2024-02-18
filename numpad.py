425105a4484c1b790d597f95ebc0fc09ef409b14
EvinExiNiTy <javadfeyux@gmail.com>
2024-02-18 19:59:57 +0330

    Compability Issue Fixed.
    Fixed Having to change numpad.py just to use the app in Linux & Mac Issue.
    
    Signed-off-by: EvinExiNiTy <javadfeyux@gmail.com>

--- numpad.py
+++ numpad.py
@@ -1,12 +1,25 @@
 from random import randint 
 from time import time
-from os import system
+from os import system, name
 from colorama import Back, init
+
 init(autoreset=True)
 tre = 0
 times = int(input("how much number do you want to take for exam : "))
 scor = 0
-system("CLS")
+os_base = name # Using name from os Module, to get User's Operating System ( Windows: nt, Linux: posix, not sure about mac but it may be posix also. )
+if os_base == 'nt':
+    # IF Using Microsoft Windows
+    system("CLS")
+
+elif os_base == 'posix':
+    # IF Using MacOS and Linux
+    system("clear")
+
+else:
+    # IF Using Anything Else that is Unknown...
+    print("Your Operating System is Unknown to this Program, Clearing your Terminal Won't be Available.")
+
 for i in range(times):
     num = randint(1000000 , 10000000)
     print(Back.GREEN + str(num))
@@ -22,4 +35,4 @@ for i in range(times):
         
 print(f"your score is : {scor}/{times}")
 print(f"avrage time for typing {scor} num : {tre/scor}")
-input()  
+input()
